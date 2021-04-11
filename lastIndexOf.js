bb1ace7e84a9d6aa413da2d08647e63b734848f4
Senayson <senaialema@gmail.com>
2021-04-11 19:31:18 +0000

    Index of last element

---
+++ lastIndexOf.js
@@ -0,0 +1,17 @@
+const lastIndexOf = function(array, val){
+  for(let i = array.length - 1; i >= 0;i++){
+    if(array[i] === val){
+     return i;
+    } else if(array === [] || array[i] !== val ){ 
+      return -1;
+    }
+    break;
+  }
+};
+  
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
